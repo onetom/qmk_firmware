d40274b34e069145a8ff7c20e675e91bc0be88a1
Tamas Herman <tamas.herman@enuma.io>
2019-01-24 01:56:27 +0800

    QW -> ESC, GUI<>CTRL swap

---
+++ keyboards/minidox/keymaps/onetom/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define COMBO_COUNT 1
+//#define TAPPING_TERM 200
+#define COMBO_TERM 100
+
+#endif
