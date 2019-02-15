4d39bcdc769dac90a3476d7186ac2b10fde5d3ef
Tamas Herman <tamas.herman@enuma.io>
2019-02-16 05:46:05 +0800

    talljoe-gherkin derivative

--- keyboards/minidox/keymaps/onetom/config.h
+++ keyboards/minidox/keymaps/onetom/config.h
@@ -1,8 +1,9 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-//#define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
+//#define TAPPING_TOGGLE 2
+#define PERMISSIVE_HOLD
+//#define IGNORE_MOD_TAP_INTERRUPT    // It only applies to modifiers, not layer toggles
 //#define TAPPING_TERM 150
 //#define COMBO_TERM 100
 #define COMBO_COUNT 9
--- keyboards/minidox/keymaps/onetom/keymap.c
+++ keyboards/minidox/keymaps/onetom/keymap.c
@@ -4,75 +4,64 @@ extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
 #define _NUM 1
-#define _SFT 2
-#define _NAV 3
-#define _SYM 4
+#define _NAV 2
+#define _CMD 3
+#define _CTL 4
 
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#define ___T___ KC_TRNS
+#define CMD LM(_CMD, MOD_LGUI)
+#define CTL LM(_CTL, MOD_LCTL)
+#define NAV TG(_NAV)
+#define NUM_BSP LT(_NUM, KC_BSPC)
+#define NAV_SPC LT(_NAV, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QWERTY] = VERTICAL_LAYOUT( // 0
-  KC_Q,                  KC_W,                  KC_E,                  KC_R,                  KC_T,
-  KC_A,                  KC_S,                  KC_D,                  KC_F,                  KC_G,
-  MT(MOD_LCTL, KC_Z),    MT(MOD_LALT, KC_X),    LT(_NAV, KC_C),        LT(_SYM, KC_V),        LT(_SFT, KC_BSPC),
-                                                ALT_T(KC_TAB),         LT(_SFT, KC_BSPC),     GUI_T(KC_SPC),
 
-  KC_Y,                  KC_U,                  KC_I,                  KC_O,                  KC_P,
-  KC_H,                  KC_J,                  KC_K,                  KC_L,                  KC_ESC,
-  LT(_NUM, KC_SPC),      KC_B,                  MT(MOD_RALT, KC_N),    MT(MOD_RCTL, KC_M),    MT(MOD_RSFT, KC_ENT),
-  GUI_T(KC_ENT),         LT(_NUM, KC_SPC),      KC_LCTL
-),
-
-[_NUM] = VERTICAL_LAYOUT( // 1
-  KC_1,                  KC_2,                  KC_3,                  KC_4,                  KC_5,
-  KC_F1,                 KC_F2,                 KC_F3,                 KC_F4,                 KC_F5,
-  _______,               _______,               _______,               _______,               _______,
-                                                _______,               _______,               KC_DEL,
-
-  KC_6,                  KC_7,                  KC_8,                  KC_9,                  KC_0,
-  KC_F6,                 KC_F7,                 KC_F8,                 KC_F9,                 KC_F10,
-  _______,               _______,               _______,               _______,               _______,
-  _______,               _______,               _______
-),
-
-[_SFT] = VERTICAL_LAYOUT( // 2
-  LSFT(KC_1),            LSFT(KC_2),            LSFT(KC_3),            LSFT(KC_4),            LSFT(KC_5),
-  KC_F11,                KC_F12,                KC_F13,                KC_F14,                KC_F15,
-  _______,               _______,               _______,               _______,               _______,
-                                                _______,               _______,               _______,
-
-  LSFT(KC_6),            LSFT(KC_7),            LSFT(KC_8),            LSFT(KC_9),            LSFT(KC_0),
-  KC_F16,                KC_F17,                KC_F18,                KC_F19,                KC_GRV,
-  _______,               _______,               _______,               _______,               _______,
-  _______,               _______,               _______
+[_QWERTY] = LAYOUT( // 0
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_RSFT,
+    KC_Z,    KC_X,    KC_C,    KC_V,    NUM_BSP,                   NAV_SPC, KC_B,    KC_N,    KC_M,    KC_ENT,
+                               CTL,     NUM_BSP, CMD,     NAV,     NAV_SPC, KC_ESC
 ),
 
 
-[_NAV] = VERTICAL_LAYOUT( // 3
-  _______,               _______,               _______,               _______,               _______,
-  KC_TAB,                _______,               _______,               _______,               _______,
-  _______,               _______,               _______,               _______,               _______,
-                                                _______,               _______,               _______,
-
-  KC_MINS,               KC_EQL,                KC_LBRC,               KC_RBRC,               KC_BSLS,
-  KC_COMM,               KC_DOT,                KC_SLSH,               KC_SCLN,               KC_QUOT,
-  _______,               KC_LEFT,               KC_UP,                 KC_DOWN,               KC_RIGHT,
-  _______,               _______,               _______
+[_NUM] = LAYOUT( // 1
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_7,    KC_9,    KC_0,
+    KC_ESC,  KC_MINS, KC_EQL,  S(KC_9), KC_LBRC,                   KC_RBRC, S(KC_0), KC_QUOT, KC_SCLN, ___T___,
+    KC_GRV,  KC_LCTL, KC_LGUI, KC_LALT, ___T___,                   KC_TAB,  KC_BSLS, KC_COMM, KC_DOT,  KC_SLSH,
+                               _______, ___T___, ___T___, ___T___, KC_TAB,  ___T___
 ),
 
 
-[_SYM] =  VERTICAL_LAYOUT( // 4
-  _______,               _______,               _______,               _______,               _______,
-  LSFT(KC_TAB),          _______,               _______,               _______,               _______,
-  RESET,                 _______,               _______,               _______,               _______,
-                                                _______,               _______,               _______,
+[_NAV] = LAYOUT( // 2
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F15,                    KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC__VOLUP,
+    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F14,                    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, ___T___,
+    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,                    ___T___, KC_LALT, KC_LGUI, KC_LCTL, KC__VOLDOWN,
+                               RESET,   KC_DEL,  ___T___, ___T___, ___T___, ___T___
+),
 
-  LSFT(KC_MINS),         LSFT(KC_EQL),          LSFT(KC_LBRC),         LSFT(KC_RBRC),         LSFT(KC_BSLS),
-  LSFT(KC_COMM),         LSFT(KC_DOT),          LSFT(KC_SLSH),         LSFT(KC_SCLN),         LSFT(KC_QUOT),
-  _______,               KC_HOME,               KC_PGUP,               KC_PGDN,               KC_END,
-  _______,               _______,               _______
+
+[_CMD] = LAYOUT( // 3
+    KC_1,    KC_2,    KC_E,    KC_R,    _______,                   _______, _______, _______, KC_9,    KC_0,
+    KC_ESC,  KC_GRV, S(KC_TAB), KC_TAB, _______,                   _______, _______, _______, _______, ___T___,
+    KC_Z,    KC_X,    KC_C,    KC_V,    _______,                   NAV_SPC, _______, _______, _______, _______,
+                               _______, _______, ___T___, ___T___, NAV_SPC, ___T___
+),
+
+[_CTL] = LAYOUT( // 4
+    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
+    KC_ESC,  _______, S(KC_TAB),KC_TAB, _______,                   _______, _______, _______, _______, ___T___,
+    _______, _______, _______, _______, _______,                   NAV_SPC, _______, _______, _______, _______,
+                               _______, _______, ___T___, ___T___, NAV_SPC, ___T___
 )
+
+//[_EMPTY] = LAYOUT( // 15
+//    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
+//    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, ___T___,
+//    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,
+//                               _______, _______, ___T___, ___T___, _______, _______
+//)
+
 };
 
 void persistant_deifault_layer_set(uint16_t default_layer) {
